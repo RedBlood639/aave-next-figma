355a7ee603869c37a6bc74cb7edfad09c877cba5
RedBlood639 <albinorabbithunder@gamil.com>
2021-12-14 00:37:55 +0900

    Fix: Code Optimization

--- src/components/Pagination/PaginationPart.jsx
+++ src/components/Pagination/PaginationPart.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
 
 // import component
-import Pagination from './Pagination'
+import Pagination from "./Pagination";
 
-import data from './data.json'
+import data from "./data.json";
 
 // import style
-import styles from './paginationpart.module.css'
+import styles from "./paginationpart.module.css";
 
 // import image
-import Divide from '../../assets/divide1.png'
+import Divide from "../../assets/divide1.png";
 
 const PaginationPart = () => {
-    return (
-        <div className={styles.paginationpart}>
-            <div className={styles.header}>
-                <div className={styles.container}>
-                    <div className={styles.left}>
-                        <div className={styles.totaltitle}>Total Market Size</div>
-                        <div className={styles.totalsize}>$ 117,604.11Y</div>
-                    </div>
-                    <div className={styles.right}>
-                        <div className={styles.usd}>USD</div>
-                        <Image src={Divide} alt="Divide" width={1} height={25.5} />
-                        <div className={styles.native}>Native</div>
-                    </div>
-                </div>
-            </div>
-            <Pagination data={data} rowsPerPage={10} />
+  return (
+    <div className={styles.paginationpart}>
+      <div className={styles.header}>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <div className={styles.totaltitle}>Total Market Size</div>
+            <div className={styles.totalsize}>$ 117,604.11Y</div>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.usd}>USD</div>
+            <Image src={Divide} alt="Divide" width={1} height={25.5} />
+            <div className={styles.native}>Native</div>
+          </div>
         </div>
-    )
-}
+      </div>
+      <Pagination data={data} rowsPerPage={10} />
+    </div>
+  );
+};
 
-export default PaginationPart
+export default PaginationPart;
